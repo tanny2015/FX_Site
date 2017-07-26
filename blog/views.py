d599de8814c2035deabe64aea0a7a99dbfd0d4e1
tanny <tanorigin@gmail.com>
2017-07-26 22:55:45 +0800

    第一次使用template

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse(123)  # 这个括号里边是什么，网页上就显示什么
+    return render(request, 'home.html', {}) # {}里边是参数
