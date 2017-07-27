c5106f77501b49e521c8150a97660beb8d7ceb3f
tanny <tanorigin@gmail.com>
2017-07-27 13:34:25 +0800

    模板交互2

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext
+import datetime
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {}) # {}里边是参数
+    # return render(request, 'home.html', {}) # {}里边是参数
+    # render_to_response 里淘汰了context_instance，详细看这里：https://stackoverflow.com/questions/38739422/django-error-render-to-response-got-an-unexpected-keyword-argument-context-i
+    context = {'time':datetime.datetime.now()}
+    return render(template_name='home.html', context=context,request=request)
+
+
+
+
